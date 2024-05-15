bbd4f7122e56870ca35a916f8c44965379bbc1dd
Deflation8449 <169663220+Deflation8449@users.noreply.github.com>
2024-05-15 07:01:14 -0400

    fix: poggers 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload (poggers 2).
+          Edit <code>src/App.js</code> and save to reload (poggers 3).
         </p>
         <a
           className="App-link"
